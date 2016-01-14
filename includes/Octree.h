943104f0cf6f9efc194b210834469bbbebe0b9c8
Johan Nilsson Hansen <granen@hotmail.com>
2016-01-15 00:13:53 +0100

    structured header file for octree

--- includes/Octree.h
+++ includes/Octree.h
@@ -16,32 +16,28 @@ class Triangle;
 const int MAX_DEPTH = 8;
 const int MAX_CHILDREN = 16;
 
-
-
 class Octree {
 public:
 	Octree();
 	Octree(float3 origin, float3 halfVector, int depth);
-	~Octree(void);
-
-	bool hasChildren(void);
+	~Octree();
 
 	void insertTriangle(Triangle* t);
 	void insertAll(std::vector<Triangle*> &triangles);
 
-
-
+	bool hasChildren();
 	void getChildren(std::vector<Octree*>* octs);
-    std::vector<Triangle*> *getTriangles();
+	void clearChildren();
 
 	int getOctantContainingPoint(const float3& point);
 	int getTriangleCount();
 	int getTriangleCountRecursively();
+	std::vector<Triangle*> *getTriangles();
+
 	bool rayCastIntersectsAABB(float3 rayOrigin, float3 rayVector);
 	void getTrianglesInsersectedByRayCast(float3 rayOrigin, float3 rayVector, std::vector<Triangle*> *tsp);
-    AABB* getAABB();
 
-	void clearChildren();
+    AABB* getAABB();
 	void setupAABB();
 
 private:
