470c5320b32eabb2afb6b15750d210c899f0a9d4
Johan Nilsson Hansen <granen@hotmail.com>
2016-01-15 00:26:40 +0100

    Started structuring bfbroadphase

--- includes/BFBroadPhase.h
+++ includes/BFBroadPhase.h
@@ -16,6 +16,10 @@ public:
     virtual CollisionPairList computeCollisionPairs() ;
     virtual void updateCollision();
 private:
+    bool isPossiblyColliding(GameObject* gameObject1, GameObject* gameObject2);
+    void computeExactCollision(CollisionPairList possibleCollision);
+    void triggerObjectEvent(GameObject* object1, GameObject* object2, EventType eventType);
+
     std::vector<GameObject*> GameObjectList;
     std::vector<std::pair<int, int>> collidingList;
     void removeDirty();
--- src/collision/BFBroadPhase.cpp
+++ src/collision/BFBroadPhase.cpp
@@ -35,7 +35,48 @@ void BFBroadPhase::updateCollision() {
     removeDirty();
 
     CollisionPairList possibleCollision = computeCollisionPairs();
+    computeExactCollision(possibleCollision);
 
+    timer.stop();
+    stringstream timeMessage;
+    timeMessage << "Tested collision in : " << timer.getElapsedTime() << " ms";
+    Logger::logDebug(timeMessage.str());
+}
+
+
+CollisionPairList BFBroadPhase::computeCollisionPairs() {
+    CollisionPairList collisionPairs;
+
+    for (auto i = GameObjectList.begin(); i != GameObjectList.end(); i++) {
+        for (auto j = i + 1; j != GameObjectList.end(); j++) {
+            GameObject* gameObject1 = *i;
+            GameObject* gameObject2 = *j;
+
+            if(isPossiblyColliding(gameObject1,gameObject2)) {
+                collisionPairs.push_back(std::pair<GameObject *, GameObject *>(gameObject1, gameObject2));
+            }
+
+        }
+    }
+    return collisionPairs;
+}
+
+
+bool BFBroadPhase::isPossiblyColliding(GameObject* gameObject1, GameObject* gameObject2) {
+    if(!gameObject1->isDynamicObject() && !gameObject2->isDynamicObject()
+       || gameObject1->isDirty() || gameObject2->isDirty()) {
+        return false;
+    }
+
+    AABB aabb1 = gameObject1->getAABB();
+    AABB aabb2 = gameObject2->getAABB();
+
+    if(gameObject1 != gameObject2 && AabbAabbintersection(&aabb1, &aabb2)) {
+        return true;
+    }
+}
+
+void BFBroadPhase::computeExactCollision(CollisionPairList possibleCollision) {
     for(auto i = possibleCollision.begin(); i != possibleCollision.end(); i++ ) {
         CollisionPair pair = *i;
 
@@ -52,17 +93,16 @@ void BFBroadPhase::updateCollision() {
                             std::find(collidingList.begin(), collidingList.end(), std::pair<int, int>(object1->getId(), object2->getId())) != collidingList.end();
         if(octreeOctreeIntersection(object1Oct,&object1ModelMatrix,object2Oct, &object2ModelMatrix)) {
             if(wasColliding) {
-                object1->callEvent(EventType::DuringCollision,object2->getType());
-                object2->callEvent(EventType::DuringCollision,object1->getType());
+                triggerObjectEvent(object1, object2, EventType::DuringCollision);
             } else {
-                object1->callEvent(EventType::BeforeCollision,object2->getType());
-                object2->callEvent(EventType::BeforeCollision,object1->getType());
+                triggerObjectEvent(object1, object2, EventType::BeforeCollision);
+
                 collidingList.push_back(std::pair<int, int> (object1->getId(), object2->getId()));
                 collidingList.push_back(std::pair<int, int> (object2->getId(), object1->getId()));
             }
         } else if (wasColliding) {
-            object1->callEvent(EventType::AfterCollision, object2->getType());
-            object2->callEvent(EventType::AfterCollision, object1->getType());
+            triggerObjectEvent(object1,object2, EventType::AfterCollision);
+
             collidingList.erase(std::remove(collidingList.begin(), collidingList.end(),
                                             std::pair<int, int>(object1->getId(), object2->getId())));
             collidingList.erase(std::remove(collidingList.begin(), collidingList.end(),
@@ -70,36 +110,10 @@ void BFBroadPhase::updateCollision() {
         }
 
     }
-
-    timer.stop();
-
-    stringstream timeMessage;
-    timeMessage << "Tested collision in : " << timer.getElapsedTime() << " ms";
-    Logger::logDebug(timeMessage.str());
 }
 
-CollisionPairList BFBroadPhase::computeCollisionPairs() {
-    CollisionPairList collisionPairs;
-
-    for (auto i = GameObjectList.begin(); i != GameObjectList.end(); i++) {
-        for (auto j = i + 1; j != GameObjectList.end(); j++) {
-            GameObject* gameObject1 = *i;
-            GameObject* gameObject2 = *j;
-
-            if(!gameObject1->isDynamicObject() && !gameObject2->isDynamicObject()
-                    || gameObject1->isDirty() || gameObject2->isDirty()) {
-                continue;
-            }
-
-            AABB aabb1 = gameObject1->getAABB();
-            AABB aabb2 = gameObject2->getAABB();
-
-            if(gameObject1 != gameObject2 && AabbAabbintersection(&aabb1, &aabb2)) {
-                collisionPairs.push_back(std::pair<GameObject*,GameObject*>(gameObject1, gameObject2));
-            }
-        }
-    }
-    return collisionPairs;
+void BFBroadPhase::triggerObjectEvent(GameObject* object1, GameObject* object2, EventType eventType) {
+    object1->callEvent(eventType, object1->getType());
+    object2->callEvent(eventType, object2->getType());
 }
-
 
